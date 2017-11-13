29ec9fb0b4226c6cfffc6f318e2c4b0ef66a752b
Muktaka Joshipura <muktaka2000@gmail.com>
2017-11-13 07:29:47 +0530

    refactor: Attempted changing a few names.

--- PONG.py
+++ PONG.py
@@ -1,5 +1,6 @@
-import pygame
 import sys
+
+import pygame
 from pygame.locals import *
 
 player1 = raw_input("Player on the left please enter your name : ")
@@ -8,21 +9,24 @@ print
 print player1, "use W for UP and S for DOWN"
 print player2, "use P for UP and L for DOWN"
 print
-print "The player who gets a SCORE of 5 first wins"
+print "The player who gets a score of 5 first wins"
 print
 print "ALL THE BEST!!"
 
 FPS = 250
 
-LineThickness = 10
-size = 100
+LINE_THICKNESS = 10
+SIZE = 100
 
-Red = (255, 0, 0)
-Green = (0, 255, 0)
-Blue = (0, 0, 255)
-Black = (0, 0, 0)
-Orange = (255, 165, 0)
-White = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+ORANGE = (255, 165, 0)
+WHITE = (255, 255, 255)
+
+DISPLAYSURF = pygame.display.set_mode((1200, 600))
+FONT_SIZE = 20
 
 score1 = 0
 score2 = 0
@@ -31,21 +35,19 @@ pygame.time.wait(3000)
 
 def Game(score1, score2):
     pygame.init()
-    global DISPLAYSURF
-    global nfont, fontsize
-    fontsize = 20
-    nfont = pygame.font.Font('freesansbold.ttf', fontsize)
+    global nfont
+    nfont = pygame.font.Font('freesansbold.ttf', FONT_SIZE)
     FPSCLOCK = pygame.time.Clock()
-    DISPLAYSURF = pygame.display.set_mode((1200, 600))
+    
     pygame.display.set_caption('PONG')
-    ballX = 600 - (LineThickness / 2)
-    ballY = 300 - (LineThickness / 2)
-    p1 = (600 - size) / 2
-    p2 = (600 - size) / 2
+    ballX = 600 - (LINE_THICKNESS / 2)
+    ballY = 300 - (LINE_THICKNESS / 2)
+    p1 = (600 - SIZE) / 2
+    p2 = (600 - SIZE) / 2
     ballxdir = -1
     ballydir = -1
-    paddle1 = pygame.Rect(30, p1, 15, size)
-    paddle2 = pygame.Rect(1160, p2, 15, size)
+    paddle1 = pygame.Rect(30, p1, 15, SIZE)
+    paddle2 = pygame.Rect(1160, p2, 15, SIZE)
     ball = pygame.Rect(ballX, ballY, 12, 12)
     Arena()
     Paddle_Draw(paddle1)
@@ -73,11 +75,11 @@ def Game(score1, score2):
         Paddle_Draw(paddle1)
         Paddle_Draw(paddle2)
         Ball_Draw(ball)
-        scoredisplay(score1, score2)
+        score_display(score1, score2)
         ball = Moveball(ball, ballxdir, ballydir)
         ballxdir, ballydir = Edgecollision(ball, ballxdir, ballydir)
         ballxdir = paddlecollision(ball, ballxdir, paddle1, paddle2)
-        calscore(ball, score1, score2, player1, player2)
+        cal_score(ball, score1, score2, player1, player2)
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
@@ -102,19 +104,19 @@ def paddlecollision(ball, ballxdir, paddle1, paddle2):
     return ballxdir
 
 
-def scoredisplay(score1, score2):
-    score1disp = nfont.render('Score = %s' % (score1), True, White)
+def score_display(score1, score2):
+    score1disp = nfont.render('Score = %s' % (score1), True, WHITE)
     score1rect = score1disp.get_rect()
     score1rect.topleft = (100, 25)
     DISPLAYSURF.blit(score1disp, score1rect)
 
-    score2disp = nfont.render('Score = %s' % (score2), True, White)
+    score2disp = nfont.render('Score = %s' % (score2), True, WHITE)
     score2rect = score1disp.get_rect()
     score2rect.topleft = (700, 25)
     DISPLAYSURF.blit(score2disp, score2rect)
 
 
-def calscore(ball, score1, score2, player1, player2):
+def cal_score(ball, score1, score2, player1, player2):
     if ball.left == 10:
         score2 += 1
         if score2 == 5:
@@ -122,7 +124,7 @@ def calscore(ball, score1, score2, player1, player2):
             print player1, "LOSES"
             print
             print "GAME OVER"
-            scoredisplay(score1, score2)
+            score_display(score1, score2)
             pygame.quit()
             sys.exit()
         else:
@@ -134,7 +136,7 @@ def calscore(ball, score1, score2, player1, player2):
             print player2, "LOSES"
             print
             print "GAME OVER"
-            scoredisplay(score1, score2)
+            score_display(score1, score2)
             pygame.quit()
             sys.exit()
         else:
@@ -142,25 +144,25 @@ def calscore(ball, score1, score2, player1, player2):
 
 
 def Arena():
-    DISPLAYSURF.fill((0, 0, 0))
-    pygame.draw.rect(DISPLAYSURF, Orange,
-                     ((0, 0), (600, 600)), LineThickness / 4)
-    pygame.draw.rect(DISPLAYSURF, Orange, ((600, 0),
-                                           (600, 600)), LineThickness / 4)
-    pygame.draw.rect(DISPLAYSURF, Blue,
-                     ((0, 0), (1200, 600)), LineThickness * 2)
+    DISPLAYSURF.fill(BLACK)
+    pygame.draw.rect(DISPLAYSURF, ORANGE,
+                     ((0, 0), (600, 600)), LINE_THICKNESS / 4)
+    pygame.draw.rect(DISPLAYSURF, ORANGE, ((600, 0),
+                                           (600, 600)), LINE_THICKNESS / 4)
+    pygame.draw.rect(DISPLAYSURF, BLUE,
+                     ((0, 0), (1200, 600)), LINE_THICKNESS * 2)
 
 
 def Ball_Draw(ball):
-    pygame.draw.rect(DISPLAYSURF, Green, ball)
+    pygame.draw.rect(DISPLAYSURF, GREEN, ball)
 
 
 def Paddle_Draw(paddle):
-    if paddle.bottom > 600 - LineThickness:
-        paddle.bottom = 600 - LineThickness
-    elif paddle.top < LineThickness:
-        paddle.top = LineThickness
-    pygame.draw.rect(DISPLAYSURF, Red, paddle)
+    if paddle.bottom > 600 - LINE_THICKNESS:
+        paddle.bottom = 600 - LINE_THICKNESS
+    elif paddle.top < LINE_THICKNESS:
+        paddle.top = LINE_THICKNESS
+    pygame.draw.rect(DISPLAYSURF, RED, paddle)
 
 
 Game(score1, score2)
