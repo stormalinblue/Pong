3bdb0a06b1e4483fc202fa6970c1a13787438db1
Muktaka Joshipura <muktaka2000@gmail.com>
2017-11-20 01:55:51 +0530

    feat: Added leaderboard navigation.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from eventmanager import EventManager
-from basicpages import Page, SplashScreen, TextInput, LeaderBoard
+from basicpages import Page, SplashScreen, TextInput, LeaderBoardPage
 from boardpage import BoardPage
 from colors import BLACK
 from properties import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
@@ -28,12 +28,15 @@ class Browser(object):
         def cb1(value):
             self.name_1 = value
             self.get_shower(TextInput, "Enter Player 2's name", cb2)()
+            
+        def finish_cb():
+            self.get_shower(LeaderBoardPage)()
         
         def cb2(value):
             self.name_2 = value
-            self.get_shower(BoardPage, (self.name_1, self.name_2))()
+            self.get_shower(BoardPage, (self.name_1, self.name_2), finish_cb)()
         
-        self.event_manager.add_timer(0.5, self.get_shower(TextInput, "Enter Player 1's name", cb1))
+        self.event_manager.add_timer(2, self.get_shower(TextInput, "Enter Player 1's name", cb1))
 
         while True:
             self.event_manager.run()
